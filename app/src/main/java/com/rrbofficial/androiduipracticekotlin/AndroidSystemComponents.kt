ef4d1ff705b29ea38f4874371860059df2bc03d3
Rohit Balage <thatcomputerboy26@gmail.com>
2024-06-01 14:19:14 -0400

    added GIF in android sys components

--- app/src/main/java/com/rrbofficial/androiduipracticekotlin/AndroidSystemComponents.kt
+++ app/src/main/java/com/rrbofficial/androiduipracticekotlin/AndroidSystemComponents.kt
@@ -1,10 +1,14 @@
 package com.rrbofficial.androiduipracticekotlin
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
 
 class AndroidSystemComponents : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +16,29 @@ class AndroidSystemComponents : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_android_system_components)
 
+        // Initialize the Toolbar
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        // Set navigation icon
+//        toolbar.setNavigationIcon(R.drawable.profilepic)
+
+        // Handle navigation icon click
+        toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+
+        // for gif image
+        val gifImageView: ImageView = findViewById(R.id.gifComponents)
+        Glide.with(this)
+            .load(R.drawable.androidsysgif)
+            .into(gifImageView)
+    }
+
+    override fun onBackPressed() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        super.onBackPressed()
     }
 }
--- app/src/main/java/com/rrbofficial/androiduipracticekotlin/MainActivity.kt
+++ app/src/main/java/com/rrbofficial/androiduipracticekotlin/MainActivity.kt
@@ -35,6 +35,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         binding.AritificalIntelligence.setOnClickListener(this)
         binding.IOTandThings.setOnClickListener(this)
         binding.AdvanceUIComponets.setOnClickListener(this)
+        binding.AndroidSystemComponents.setOnClickListener(this)
 
 
 
