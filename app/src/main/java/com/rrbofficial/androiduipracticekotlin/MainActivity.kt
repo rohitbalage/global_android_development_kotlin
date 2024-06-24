2d1373fda5f5d5d0bd183363bf5bad1568d7e9ab
Rohit Balage <thatcomputerboy26@gmail.com>
2024-06-24 00:16:02 -0400

    changed configuration for the app to mim sdk 21

--- app/src/main/java/com/rrbofficial/androiduipracticekotlin/MainActivity.kt
+++ app/src/main/java/com/rrbofficial/androiduipracticekotlin/MainActivity.kt
@@ -42,9 +42,32 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 // TODO: Inform user that that your app will not show notifications.
             }
         }
+        fun askNotificationPermission() {
+            // This is only necessary for API level >= 33 (TIRAMISU)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                if (ContextCompat.checkSelfPermission(
+                        this,
+                        android.Manifest.permission.POST_NOTIFICATIONS
+                    ) ==
+                    PackageManager.PERMISSION_GRANTED
+                ) {
+                    // FCM SDK (and your app) can post notifications.
+                } else if (shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
+                    // TODO: display an educational UI explaining to the user the features that will be enabled
+                    //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
+                    //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+                    //       If the user selects "No thanks," allow the user to continue without notifications.
+                } else {
+                    // Directly ask for the permission
+                    requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+                }
+            }
+
+        }
 
 
-        // Data binding initialization
+
+            // Data binding initialization
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         // Set click listeners for all buttons using binding object
